d15c8d11619c2fd826e08356180c3de3a46594d3
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-05-05 01:52:03 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,32 @@ const PORT = 4000;
 
 const app = express();
 
-app.get("/", (req, res) => res.send("<h1>Hello, World!</h1>"));
+const urlLogger = (req, res, next) => {
+    console.log(`Path: ${req.path}`);
+    next();
+  };
+  
+  const timeLogger = (req, res, next) => {
+    const time = new Date();
+    console.log(`Time: ${time.getFullYear()}.${time.getMonth()+1}.${time.getDate()}`);
+    next();
+  };
+  
+  const securityLogger = (req, res, next) => {
+    if(req.protocol==="https") console.log("Secure");
+    else console.log("Insecure");
+    next();
+  };
+  
+  const protector = (req, res, next) => {
+    if (req.path === "/protected") return res.end();
+    next();
+  };
+  
+  app.use(urlLogger, timeLogger, securityLogger, protector);
+  
+  app.get("/", (req, res) => res.send("<h1>Home</h1>"));
+  app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
