961c572a0729813037b4b9f1d70e63db94dfc001
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-05-05 11:42:40 +0900

    morgan

--- src/server.js
+++ src/server.js
@@ -1,36 +1,16 @@
 import express from "express"
+import morgan from "morgan"
 // const express = require("express");
 
 const PORT = 4000;
 
 const app = express();
 
-const urlLogger = (req, res, next) => {
-    console.log(`Path: ${req.path}`);
-    next();
-  };
+const logger = morgan("dev");
   
-  const timeLogger = (req, res, next) => {
-    const time = new Date();
-    console.log(`Time: ${time.getFullYear()}.${time.getMonth()+1}.${time.getDate()}`);
-    next();
-  };
-  
-  const securityLogger = (req, res, next) => {
-    if(req.protocol==="https") console.log("Secure");
-    else console.log("Insecure");
-    next();
-  };
-  
-  const protector = (req, res, next) => {
-    if (req.path === "/protected") return res.end();
-    next();
-  };
-  
-  app.use(urlLogger, timeLogger, securityLogger, protector);
-  
-  app.get("/", (req, res) => res.send("<h1>Home</h1>"));
-  app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
+app.use(logger);
+app.get("/", (req, res) => res.send("<h1>Home</h1>"));
+app.get("/protected", (req, res) => res.send("<h1>Protected</h1>"));
 
 const handleListening = () => console.log(`Server listening on port http://localhost:${PORT}`);
 
