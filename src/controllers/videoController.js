22fc3deb660f0dfeebb367d4ace977384359c3cf
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-05-11 19:10:10 +0900

    templates practice

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,30 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+const fakeUser = {
+    username: "Me",
+    loggedIn: true
+};
+const videos = [
+    {title: "sweet home",
+     rating: 5,
+     comments: 10,
+     createdAt: "me",
+     views: 100},
+    {title: "sweet home",
+     rating: 5,
+     comments: 10,
+     createdAt: "me",
+     views: 100},
+    {title: "sweet home",
+     rating: 5,
+     comments: 10,
+     createdAt: "me",
+     views: 100},
+    {title: "sweet home",
+     rating: 5,
+     comments: 10,
+     createdAt: "me",
+     views: 100},
+];
+export const trending = (req, res) => res.render("home", {pageTitle: "Home", fakeUser: fakeUser, videos: videos});
 export const see = (req, res) => res.render("watch", {pageTitle: "Watch"});
 export const edit = (req, res) => res.render("edit", {pageTitle: "Edit"});
 export const upload = (req, res) => res.send("Upload video")
