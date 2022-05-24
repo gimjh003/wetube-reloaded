6a91cd6e937846c024f47325e6791942b710df15
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-05-24 16:44:29 +0900

    env setting complete

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import morgan from "morgan";
 import {localsMiddleware} from "./middlewares";
 import rootRouter from "./routers/rootRouter";
@@ -16,7 +17,11 @@ app.set("views", process.cwd()+"/src/views");
 app.set("x-powered-by", false);
 app.use(logger);
 app.use(express.urlencoded({extended: true}));
-app.use(session({secret: "Hello!", resave: true, saveUninitialized: true}));
+app.use(session({secret: process.env.COOKIE_SECRET, 
+                 resave: false, 
+                 saveUninitialized: false,
+                 store: MongoStore.create({mongoUrl: process.env.DB_URL}),
+                }));
 app.use(localsMiddleware)
 app.use("/", rootRouter);
 app.use("/videos", videoRouter);
