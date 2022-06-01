0bef8bf9fa50910c20bb21932648bda023ce232d
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-06-01 19:22:40 +0900

    Update video recorder

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,51 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async() => {
-    const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+let videoFile;
+
+const init = async() => {
+    stream = await navigator.mediaDevices.getUserMedia({
         audio: true,
         video: true,
     });
     video.srcObject = stream;
     video.play();
+    return;
+}
+
+init();
+
+const handleDownload = () => {
+    const a = document.createElement("a");
+    a.href = videoFile;
+    a.download = "recording.wav";
+    document.body.appendChild(a);
+    a.click();
+}
+
+const handleStop = () => {
+    recorder.stop();
+    startBtn.innerText = "Download Recording";
+    startBtn.removeEventListener("click", handleStop);
+    startBtn.addEventListener("click", handleDownload);
+    return;
+}
+
+const handleStart = () => {
+    startBtn.innerText = "Stop Recording";
+    startBtn.removeEventListener("click", handleStart);
+    startBtn.addEventListener("click", handleStop);
+    recorder = new MediaRecorder(stream);
+    recorder.ondataavailable = (e) => {
+        videoFile = URL.createObjectURL(e.data);
+        video.srcObj = null;
+        video.src = videoFile;
+        video.loop = true;
+        video.play();
+    }
+    recorder.start();
     return;
 }
 
