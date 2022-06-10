60dda33a404cda8482454732bffa40b4d65a49c0
gimjh003 <46261152+gimjh003@users.noreply.github.com>
2022-06-11 02:14:36 +0900

    heroku env

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import User from "./models/User";
 import Comment from "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => console.log(`Server listening on http://localhost:${PORT}`);
 app.listen(PORT, handleListening);
